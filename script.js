cc545e2082d70ac6eddd4514cc1f3cdb9eebedc3
Anoop Singh <95135696+anoop7384@users.noreply.github.com>
2022-07-20 13:43:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-console.log("Welcome");
+console.log("Welcome :)");
 const API_KEY = "api_key=ccb1221ca78bf9e8a9b25e38f3a0d00f";
 const Base_URL = "https://api.themoviedb.org/3";
 const API_URL = Base_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = Base_URL + '/search/movie?' + API_KEY;
 
+//SEARCH BAR LOGIC
 const search = document.getElementById('searchTxt');
 search.addEventListener("input", async function () {
     let inputVal = search.value.toLowerCase();
@@ -17,18 +18,18 @@ search.addEventListener("input", async function () {
     else{
         display(results);
     }
-
 });
 
+//SHOWING MOVIE CARDS
 function display(data) {
     let grid = document.getElementById('grid');
     grid.innerHTML = "";
     data.forEach(element => {
         const string = `
-        <div class=" movie card flex flex-col items-center  rounded-lg " style="height: 25rem; width: 30rem; margin-bottom: 4rem;">
-            <img class="" id="logo" src="${IMG_URL + element.poster_path}" alt="" srcset="" style="height: 20rem; width: 30rem">
+        <div class=" movie card flex flex-col items-center  rounded-lg " style="height: 30rem; width: 30rem; margin-bottom: 4rem;">
+            <img class="" id="logo" src="${IMG_URL + element.poster_path}" alt="" srcset="" style="height: 25rem; width: 30rem">
             <div style="display: flex;
-            justify-content: center; text-align: center; height: 5rem;"><p id="title" class="text-3xl mt-4 text-black">${element.title}</p></div>
+            justify-content: center; text-align: center; height: 5rem;"><p id="title" class="text-3xl mt-4 text-black"><b>${element.title}</b></p></div>
             
         </div>
         `;
@@ -37,7 +38,7 @@ function display(data) {
 }
 
 
-
+//FOR MAIN PAGE
 async function getData(api) {
     const resp = await fetch(api).then(response => response.json());
     const data = resp.results;
@@ -45,4 +46,5 @@ async function getData(api) {
 }
 
 getData(API_URL);
+
 
